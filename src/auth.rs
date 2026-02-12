76f9a15b947f517d5be4f15096b06494f9ab594e
Ferivonus <ferivonus@hotmail.com>
2026-02-12 23:01:13 +0300

    code fix

--- src/auth.rs
+++ src/auth.rs
@@ -4,13 +4,10 @@ use std::future::{ready, Ready};
 use uuid::Uuid;
 
 use crate::models::{AppState, Claims, UserRole};
-
-// Bu struct, korumalı rotalara (handler'lara) parametre olarak
-// enjekte edilecek olan, kimliği doğrulanmış kullanıcıdır.
 pub struct AuthenticatedUser {
     pub id: Uuid,
     pub username: String,
-    pub role: UserRole, // String yerine artık Enum kullanıyoruz
+    pub role: UserRole,
 }
 
 impl FromRequest for AuthenticatedUser {
@@ -18,32 +15,27 @@ impl FromRequest for AuthenticatedUser {
     type Future = Ready<Result<Self, Self::Error>>;
 
     fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        // 1. AppState'e (Veritabanı ve Secret Key'e) ulaş
         let data = req.app_data::<web::Data<AppState>>().unwrap();
 
-        // 2. Cookie'den token'ı çek
         let auth_token = match req.cookie("auth_token") {
             Some(cookie) => cookie.value().to_string(),
             None => return ready(Err(ErrorUnauthorized("Giriş yapmanız gerekiyor."))),
         };
 
-        // 3. Token'ı çöz (Decode)
         let token_data = decode::<Claims>(
             &auth_token,
             &DecodingKey::from_secret(data.jwt_secret.as_bytes()),
             &Validation::default(),
         );
 
-        // 4. Sonucu döndür
         match token_data {
             Ok(token) => {
-                // Token içindeki ID string olduğu için UUID'ye çeviriyoruz
                 let user_id = Uuid::parse_str(&token.claims.sub).unwrap_or_default();
 
                 ready(Ok(AuthenticatedUser {
                     id: user_id,
                     username: token.claims.username,
-                    role: token.claims.role, // Enum otomatik eşleşir
+                    role: token.claims.role,
                 }))
             }
             Err(_) => ready(Err(ErrorUnauthorized("Geçersiz veya süresi dolmuş mühür."))),
